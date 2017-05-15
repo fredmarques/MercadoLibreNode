0c4b93458e9db8df3b37a05f35b69b330ef7aec7
Frederico Marques <frederico.marques.22@gmail.com>
2017-05-15 16:47:56 -0300

    implementing issue #2

---
+++ env.sh
@@ -0,0 +1,4 @@
+export api_root_url="https://api.mercadolibre.com"
+export skd_version="0.0.13"
+export auth_url="https://auth.mercadolibre.com/authorization"
+export oauth_url="https://api.mercadolibre.com/oauth/token"
